3b6befa933bddd86b4ff5fb7a1ead059f5a9525c
Brad Gibbs <bradley@thegyre.io>
2023-11-27 12:26:11 -0600

    db changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/forms/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
